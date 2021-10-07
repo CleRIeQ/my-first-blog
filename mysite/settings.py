831a649df235f9113781d17b34add9aaf7549002
CleRIeQ <aleksandrsd006@gmail.com>
2021-10-08 05:53:14 +0600

    fix b-strap and css attemp1

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,5 +123,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+# first
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
