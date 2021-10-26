dc30d5a2155b1d0f81f12d53f84461020655d33d
CleRIeQ <aleksandrsd006@gmail.com>
2021-10-26 06:01:26 +0600

    migration bag fix

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    textinfo = models.CharField(max_length=450, default=' ')
+    textinfo = models.CharField(max_length=450, default='О чем ваш текст?')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
